824d43be6b378f78be6905c0c3a1be9b6b50c1ed
kaikallastu <kai.kallastu@eesti.ee>
2018-05-23 19:37:02 +0300

    Added EFRepositories

---
+++ DAL.App.EF/Repositories/EFPerformancePerformerRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DAL.App.Interfaces.Repositories;
+using DAL.EF.Repositories;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.App.EF.Repositories
+{
+    class EFPerformancePerformerRepository : EFRepository<PerformancePerformer>, IPerformancePerformerRepository
+    {
+        public EFPerformancePerformerRepository(DbContext dataContext) : base(dataContext)
+        {
+        }
+    }
+}
---
+++ DAL.App.EF/Repositories/EFTicketRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DAL.App.Interfaces.Repositories;
+using DAL.EF.Repositories;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace DAL.App.EF.Repositories
+{
+    public class EFTicketRepository : EFRepository<Ticket>, ITicketRepository
+    {
+        public EFTicketRepository(DbContext dataContext) : base(dataContext)
+        {
+        }
+    }
+}
