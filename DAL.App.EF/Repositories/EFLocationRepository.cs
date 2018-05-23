131fc7605e2c785442bfb3eaed981eebd9476684
Kaarel <kaarelp2rtel@gmail.com>
2018-05-23 19:36:26 +0300

    Added EF Repos

---
+++ DAL.App.EF/Repositories/EFLocationRepository.cs
@@ -0,0 +1,17 @@
+﻿using DAL.App.Interfaces.Repositories;
+using DAL.EF.Repositories;
+using Domain;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.App.EF.Repositories
+{
+    public class EFLocationRepository : EFRepository<Location>, ILocationRepository
+    {
+        public EFLocationRepository(DbContext dataContext) : base(dataContext)
+        {
+        }
+    }
+}
